b1f163416e18d732490d90e4db24b6eef6049094
Dheeraj Malisetty <malisettydheeraj@gmail.com>
2017-10-26 17:00:00 +0530

    first commit

---
+++ dheeraj/mario.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(void)
+{
+    int c;
+    scanf("%d",&c);
+    for (int i=0;i<=c;i++)
+    {
+        for (int j=0;j<=c-i;j++)
+        {
+            printf(" ");
+        }
+        for (int k=i;k>=0;k--)
+        {
+            printf("#");
+        }
+        printf(" ");
+        for (int h=0;h<=i;h++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
---
+++ dheeraj/water.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void)
+{
+    int c=10;
+
+    int d=((c*1.5*128)/16);
+    printf("%d",d);
+}
