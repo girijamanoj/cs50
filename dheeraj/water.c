4c77f06d4ab9a72f1a851d5e4c9d5a726942d1b1
GirijaManoj kumar reddy Kalakoti <kgmkr00001@gmail.com>
2018-03-22 15:11:15 +0530

    Update water.c

--- dheeraj/water.c
+++ dheeraj/water.c
@@ -1,8 +1,8 @@
+//No point in writing this code as it is equivalent to print 120 Please check this once
 #include <stdio.h>
-int main(void)
+void main(void)
 {
     int c=10;
-
     int d=((c*1.5*128)/16);
     printf("%d",d);
 }
